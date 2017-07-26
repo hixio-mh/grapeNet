77c609ddf5e5789918d5f7ded73d9fce0c8efd5f
koangel <jackliu100@gmail.com>
2017-07-26 14:49:32 +0800

    增加对象数据解析类

---
+++ Codec/Codec.go
@@ -0,0 +1,54 @@
+// 自动根据注册类型根据协议结构反射对应类型
+// 线程安全类型
+// 通过类型名称快速New一个对象指针
+// version 1.0 beta
+// by koangel
+// email: jackliu100@gmail.com
+// 2017/7/26
+package grapeCodec
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+var objmap map[string]reflect.Type = make(map[string]reflect.Type)
+var locker sync.Mutex
+
+func RA(val interface{}) {
+	t := reflect.TypeOf(val)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	R(fmt.Sprintln(t), val)
+}
+
+func R(name string, val interface{}) {
+	locker.Lock()
+	defer locker.Unlock()
+
+	_, ok := objmap[name]
+	if ok {
+		delete(objmap, name)
+	}
+
+	objmap[name] = reflect.TypeOf(val)
+}
+
+func New(name string) (o interface{}, err error) {
+	locker.Lock()
+	defer locker.Unlock()
+	err = nil
+	o = nil
+	if v, ok := objmap[name]; ok {
+		o = reflect.New(v).Interface()
+	} else {
+		err = errors.New("Unregister Codec Object...")
+	}
+
+	return
+}
---
+++ Codec/Codec_test.go
@@ -0,0 +1,44 @@
+package grapeCodec
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type VTObject struct {
+	Name string
+	VAO  int
+	Data string
+}
+
+func Test_Codec(t *testing.T) {
+
+	TempV := &VTObject{
+		Name: "asdasd",
+		VAO:  2000,
+		Data: "azxzxczxc",
+	}
+	v, verr := json.Marshal(TempV)
+	if verr != nil {
+		t.Error(verr)
+		return
+	}
+
+	RA(VTObject{})
+	R("VTest", VTObject{})
+	obj, err := New("VTest")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(obj, reflect.TypeOf(obj))
+	if err = json.Unmarshal(v, obj); err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(obj)
+}
