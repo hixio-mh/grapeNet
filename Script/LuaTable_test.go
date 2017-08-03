c954ff2ad138c6030b286aa557e754c044564d3d
koangel <jackliu100@gmail.com>
2017-08-03 16:26:42 +0800

    完善數據性能測試

--- Script/LuaTable_test.go
+++ Script/LuaTable_test.go
@@ -33,3 +33,39 @@ func Test_GetLuaData(t *testing.T) {
 	fmt.Println(person)
 	t.Log("finished...")
 }
+
+func Benchmark_LuaData(b *testing.B) {
+	lua := NewFromFile("getData", "../_lua_tests/luascripts/get_data_test.lua")
+	if lua == nil {
+		b.Error("lua do file error...")
+		return
+	}
+
+	for i := 0; i < b.N; i++ {
+		person := Person{}
+		err := lua.GetTable("person", &person)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+	}
+}
+
+func Benchmark_LuaData_Parallel(b *testing.B) {
+	lua := NewFromFile("getData", "../_lua_tests/luascripts/get_data_test.lua")
+	if lua == nil {
+		b.Error("lua do file error...")
+		return
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			person := Person{}
+			err := lua.GetTable("person", &person)
+			if err != nil {
+				b.Error(err)
+				return
+			}
+		}
+	})
+}
